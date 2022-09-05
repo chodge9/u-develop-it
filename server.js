4989b93489053df585d7a06beeb384496c98347e
Chantal Hodge <chantal_h09@yahoo.com>
2022-09-04 23:32:44 -0500

    build database calls

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,43 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'chantal',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+/*db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+}); */
+
+//Get a single candidate
+/*db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+}); */
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+             VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 /*app.get('/', (req, res)=> {
     res.json({
